08dc2eb63e79e97d1a6acba63b5785a017003b1b
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-09-24 18:06:34 +0100

    Header styles.

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,11 +9,11 @@ export default function Header() {
         <nav className="max-w-[1920px">
           <div id="sticky-menu" className="fixed w-full top-0 left-0 z-50">
             <div className="flex items-center justify-between h-24 px-5">
-              <div></div>
-              <div>
+              <div className="flex-1 text-white">Hello world!</div>
+              <div className="flex-1 grid place-items-center">
                 <Image className="w-20" src="/logo.png" alt="logo" width={205} height={205} />
               </div>
-              <div id="nav-menu" className="absolute top-0 right-0 min-h-[70ch] w-full px-5 bg-white flex flex-col gap-5 duration-500 ease-out overflow-hidden">
+              <div id="nav-menu" className="absolute top-0 right-[-100%] min-h-[70ch] w-full px-5 bg-white flex flex-col gap-5 duration-500 ease-out overflow-hidden">
                 <div className="flex items-center justify-between h-24">
                   <Image className="w-20" src="/logo_2.png" alt="logo_2" width={205} height={205} />
                   <div id="closeicon" className="text-3xl cursor-pointer">
@@ -37,36 +37,36 @@ export default function Header() {
                     <a href="#products" className="nav-link">Products</a>
                   </li>
                 </ul>
-                <div>
-                  <p>Follow us <span>-</span></p>
-                  <ul>
-                    <li>
+                <div className="flex items-center py-10">
+                  <p className="font-bold">Follow us <span className="ml-3 mr-5">-</span></p>
+                  <ul className="nav__icons__mobile flex space-x-4">
+                    <li className="border-2 border-black h-8 w-8 rounded-full grid place-items-center">
                       <a href="#">
                         <RiTwitterXLine />
                       </a>
                     </li>
-                    <li>
+                    <li className="border-2 border-black h-8 w-8 rounded-full grid place-items-center">
                       <a href="#">
                         <RiFacebookFill />
                       </a>
                     </li>
-                    <li>
+                    <li className="border-2 border-black h-8 w-8 rounded-full grid place-items-center">
                       <a href="#">
                         <RiInstagramLine />
                       </a>
                     </li>
-                    <li>
+                    <li className="border-2 border-black h-8 w-8 rounded-full grid place-items-center">
                       <a href="#">
                         <RiYoutubeFill />
                       </a>
                     </li>
                   </ul>
                 </div>
-                <div>
+                <div className="absolute bottom-16 -right-5 -rotate-[30deg] opacity-20 pointer-events-none">
                   <Image className="w-72" src="/coffee_leaf.png" alt="bg_leaf" width={205} height={205} />
                 </div>
               </div>
-              <div>
+              <div className="flex-1 text-white text-3xl flex justify-end cursor-pointer">
                 <RiMenu4Line />
               </div>
             </div>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import { Poppins } from 'next/font/google'
 import "./globals.scss"
 
-const poppins = Poppins({ subsets: ['latin'], weight: '400', style: 'normal' })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'], style: 'normal' })
 
 export const metadata = {
   title: "Brewy",
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased max-w[1920px] mx-auto bg-white text-green-950`}
+        className={`${poppins.className} antialiased max-w[1920px] mx-auto bg-black text-green-950`}
       >
         {children}
       </body>
