473939ed43ce76b52755f5522d4711c0001b198f
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-10-03 22:19:18 +0100

    Cards animation for tablets.

--- app/components/CardsSection.jsx
+++ app/components/CardsSection.jsx
@@ -14,79 +14,159 @@ export default function CardsSection() {
   const textRef = useRef(null);
 
   useGSAP(() => {
+    const mediaQueries = gsap.matchMedia()
     const cards = cardRefs.current
     const totalScrollHeight = window.innerHeight * 3
     const positions = [14, 38, 62, 86]
     const rotations = [-15, -7.5, 7.5, 15]
+    const mdPositionsX = [25, 75, 25, 75]
+    const mdPositionsY = [35, 35, 75, 75]
+    const mdRotations = [-7.5, 7.5, -7.5, 7.5]
 
-    ScrollTrigger.create({
-      trigger: containerRef.current.querySelector('.cards'),
-      start: 'top top',
-      end: () => `+=${totalScrollHeight}`,
-      pin: true,
-      pinSpacing: true,
-    })
-
-    cards.forEach((card, index) => {
-      gsap.to(card, {
-        left: `${positions[index]}%`,
-        rotation: `${rotations[index]}`,
-        top: '60%',
-        ease: 'none',
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: 'top top',
-          end: () => `+=${window.innerHeight}`,
-          scrub: .5,
-          id: `spread-${index}`
-        }
-      })
-    })
-
-    gsap.to(textRef.current, {
-      y: 0,
-      opacity: 1,
-      duration: 0.5,
-      ease: 'power1.out',
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: '4000',
-        toggleActions: 'play reverse play reverse',
-      }
-    });
-
-    cards.forEach((card, index) => {
-      const frontEl = card.querySelector('.flip-card-front')
-      const backEl = card.querySelector('.flip-card-back')
-
-      const staggerOffset = index * .05
-      const startOffset = 1 / 3 + staggerOffset
-      const endOffset = 2 / 3 + staggerOffset
-
+    mediaQueries.add('(max-width: 1023px)', () => {
       ScrollTrigger.create({
         trigger: containerRef.current.querySelector('.cards'),
         start: 'top top',
         end: () => `+=${totalScrollHeight}`,
-        scrub: 1,
-        id: `rotate-flip-${index}`,
-        onUpdate: (self) => {
-          const progress = self.progress
-          if (progress >= startOffset && progress <= endOffset) {
-            const animationProgress = (progress - startOffset) / (1 / 3)
-            const frontRotation = -180 * animationProgress
-            const backRotation = 180 - 180 * animationProgress
-            const cardRotation = rotations[index] * (1 - animationProgress)
+        pin: true,
+        pinSpacing: true,
+      })
 
-            gsap.to(frontEl, { rotateY: frontRotation, ease: 'power1.out' })
-            gsap.to(backEl, { rotateY: backRotation, ease: 'power1.out' })
-            gsap.to(card, {
-              xPercent: -50,
-              yPercent: -50,
-              rotate: cardRotation,
-              ease: 'power1.out'
-            })
+      cards.forEach((card, index) => {
+        gsap.to(card, {
+          left: `${mdPositionsX[index]}%`,
+          rotation: `${mdRotations[index]}`,
+          top: `${mdPositionsY[index]}%`,
+          ease: 'none',
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: 'top top',
+            end: () => `+=${window.innerHeight}`,
+            scrub: .5,
+            id: `spread-${index}`
           }
+        })
+      })
+
+      gsap.to(textRef.current, {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power1.out',
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: 'top 500',
+          end: '3000',
+          toggleActions: 'play reverse play reverse',
         }
+      });
+
+      cards.forEach((card, index) => {
+        const frontEl = card.querySelector('.flip-card-front')
+        const backEl = card.querySelector('.flip-card-back')
+  
+        const staggerOffset = index * .05
+        const startOffset = 1 / 3 + staggerOffset
+        const endOffset = 2 / 3 + staggerOffset
+  
+        ScrollTrigger.create({
+          trigger: containerRef.current.querySelector('.cards'),
+          start: 'top top',
+          end: () => `+=${totalScrollHeight}`,
+          scrub: 1,
+          id: `rotate-flip-${index}`,
+          onUpdate: (self) => {
+            const progress = self.progress
+            if (progress >= startOffset && progress <= endOffset) {
+              const animationProgress = (progress - startOffset) / (1 / 3)
+              const frontRotation = -180 * animationProgress
+              const backRotation = 180 - 180 * animationProgress
+              const cardRotation = rotations[index] * (1 - animationProgress)
+  
+              gsap.to(frontEl, { rotateY: frontRotation, ease: 'power1.out' })
+              gsap.to(backEl, { rotateY: backRotation, ease: 'power1.out' })
+              gsap.to(card, {
+                xPercent: -50,
+                yPercent: -50,
+                rotate: cardRotation,
+                ease: 'power1.out'
+              })
+            }
+          }
+        })
+      })
+    })
+
+    mediaQueries.add('(min-width: 1024px)', () => {
+      ScrollTrigger.create({
+        trigger: containerRef.current.querySelector('.cards'),
+        start: 'top top',
+        end: () => `+=${totalScrollHeight}`,
+        pin: true,
+        pinSpacing: true,
+      })
+  
+      cards.forEach((card, index) => {
+        gsap.to(card, {
+          left: `${positions[index]}%`,
+          rotation: `${rotations[index]}`,
+          top: '60%',
+          ease: 'none',
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: 'top top',
+            end: () => `+=${window.innerHeight}`,
+            scrub: .5,
+            id: `spread-${index}`
+          }
+        })
+      })
+  
+      gsap.to(textRef.current, {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power1.out',
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: '4000',
+          toggleActions: 'play reverse play reverse',
+        }
+      });
+  
+      cards.forEach((card, index) => {
+        const frontEl = card.querySelector('.flip-card-front')
+        const backEl = card.querySelector('.flip-card-back')
+  
+        const staggerOffset = index * .05
+        const startOffset = 1 / 3 + staggerOffset
+        const endOffset = 2 / 3 + staggerOffset
+  
+        ScrollTrigger.create({
+          trigger: containerRef.current.querySelector('.cards'),
+          start: 'top top',
+          end: () => `+=${totalScrollHeight}`,
+          scrub: 1,
+          id: `rotate-flip-${index}`,
+          onUpdate: (self) => {
+            const progress = self.progress
+            if (progress >= startOffset && progress <= endOffset) {
+              const animationProgress = (progress - startOffset) / (1 / 3)
+              const frontRotation = -180 * animationProgress
+              const backRotation = 180 - 180 * animationProgress
+              const cardRotation = rotations[index] * (1 - animationProgress)
+  
+              gsap.to(frontEl, { rotateY: frontRotation, ease: 'power1.out' })
+              gsap.to(backEl, { rotateY: backRotation, ease: 'power1.out' })
+              gsap.to(card, {
+                xPercent: -50,
+                yPercent: -50,
+                rotate: cardRotation,
+                ease: 'power1.out'
+              })
+            }
+          }
+        })
       })
     })
   }, { scope: containerRef})
