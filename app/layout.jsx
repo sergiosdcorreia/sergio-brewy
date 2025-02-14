f090de9668da270b952c486c3c450a26b56043e5
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-02-14 10:59:24 +0000

    Fixing Lenis smooth scrolling.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,13 @@
-import { Poppins } from 'next/font/google'
-import "./globals.scss"
+import { Poppins } from "next/font/google";
+import "./globals.scss";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'], style: 'normal' })
+import { ReactLenis } from "@/app/utils/lenis";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: "normal",
+});
 
 export const metadata = {
   title: "Brewy",
@@ -10,12 +16,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='w-[100vw] overflow-x-hidden'>
-      <body
-        className={`${poppins.className} antialiased max-w[1920px] mx-auto bg-black text-green-950 overflow-x-hidden`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="w-[100vw] overflow-x-hidden">
+      <ReactLenis root>
+        <body
+          className={`${poppins.className} antialiased max-w[1920px] mx-auto bg-black text-green-950 overflow-x-hidden`}
+        >
+          {children}
+        </body>
+      </ReactLenis>
     </html>
-  )
+  );
 }
